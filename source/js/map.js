6fc4f3a87dcfa4cda5d8819be99cca0422ed0e27
Stas <tokarev.st.n@gmail.com>
2020-05-20 17:35:49 +0300

    Готовый проект на проверку 20.05

---
+++ source/js/map.js
@@ -0,0 +1,24 @@
+ymaps.ready(init);
+function init(){
+  var myMap = new ymaps.Map("map", {
+    center: [59.938635, 30.323118],
+    zoom: 17,
+    controls: [],
+});
+
+let coords = [
+  [59.938635, 30.323118]
+],
+myCollection = new ymaps.GeoObjectCollection({}, {
+draggable: false,
+iconLayout: 'default#image',
+iconImageHref: './img/map-pin.png',
+iconImageSize: [82, 70],
+iconImageOffset: [-35, -52]
+});
+for (let i = 0; i < coords.length; i++) {
+  myCollection.add(new ymaps.Placemark(coords[i]));
+}
+myMap.geoObjects.add(myCollection);
+myMap.behaviors.disable('scrollZoom');
+};
